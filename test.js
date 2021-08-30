7cc11e945c069e1d0146c7ac09f0fb80e6cfc5d4
Magesh Kuppan <tkmagesh77@gmail.com>
2021-08-30 10:42:36 +0530

    Salary Calculator - initial

---
+++ test.js
@@ -0,0 +1,11 @@
+//let x : number = 100
+function add(x, y) {
+    return x + y;
+}
+console.log(add(100, 200));
+var nos = [];
+nos[0] = 10;
+//nos[1] = "Magesh"
+function subtract(x, y) {
+    return x + y;
+}
---
+++ test.ts
@@ -0,0 +1,16 @@
+//let x : number = 100
+
+function add(x : number, y : number) : number {
+    return x + y
+}
+
+console.log(add(100,200))
+
+let nos : Array<number> = []
+
+nos[0] = 10
+//nos[1] = "Magesh"
+
+function subtract(x,y){
+    return x + y;
+}

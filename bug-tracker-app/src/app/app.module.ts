dce1ecd67d1242f851110f552d175a56828a33a2
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-01 17:57:15 +0530

    refactoring to utils module

--- bug-tracker-app/src/app/app.module.ts
+++ bug-tracker-app/src/app/app.module.ts
@@ -4,27 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BugsComponent } from './bugs/bugs.component';
-import { ElapsedPipe } from './bugs/pipes/elapsed.pipe';
-import { SortPipe } from './bugs/pipes/sort.pipe';
-import { TrimTextPipe } from './bugs/pipes/trimText.pipe';
 import { ClosedCountPipe } from './bugs/pipes/closedCount.pipe';
 import { BugStatsComponent } from './bugs/components/bug-stats.component';
 import { BugEditComponent } from './bugs/components/bug-edit.component';
+import { UtilsModule } from './utils/utils.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     BugsComponent,
-    TrimTextPipe,
-    ElapsedPipe,
-    SortPipe,
     ClosedCountPipe,
     BugStatsComponent,
     BugEditComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    UtilsModule
   ],
   providers: [
    /* 

2e111ecae99e5918cf4d5e7657770ce43455868a
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-07 15:58:26 +0530

    testing saving a new bug

--- testing-demo/src/http-service/bugApi.service.spec.ts
+++ testing-demo/src/http-service/bugApi.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing'
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { BugApiService } from './bugApi.service';
+import testData from './test-data'
+import { Bug } from './bug.model';
 
 fdescribe('BugApi service', () => {
     let bugApi : BugApiService;
@@ -24,29 +26,39 @@ fdescribe('BugApi service', () => {
 
     it('should get all bugs', () => {
 
+        //arrange
+        const testBugs = testData.bugs;
+
         //act
         bugApi.getAll()
             .subscribe(bugs => {
-                expect(bugs.length).toBe(2);
+                expect(bugs.length).toBe(3);
             });
 
-        //assert
+        //assert 
         const req = httpTestingController.expectOne(bugApi.serviceEndPoint);
         expect(req.request.method).toBe('GET');
-        req.flush([
-            {
-                id : 1,
-                title : 'Bug 1',
-                description : 'Bug 1 description',
-                isClosed : false
-            },
-            {
-                id : 2,
-                title : 'Bug 2',
-                description : 'Bug 2 description',
-                isClosed : false
-            }
-        ]);
+        req.flush(testBugs);
         httpTestingController.verify();
+    });
+
+    it('should save a new bug', () => {
+        const newBugData : Bug = {
+            id : 0,
+            name : 'test bug',
+            isClosed : false,
+            createdAt : new Date()
+        };
+
+        bugApi
+            .save(newBugData)
+            .subscribe(newBug => {
+                expect(newBug.id).not.toBe(0);
+                expect(newBug.name).toBe(newBugData.name);
+            })
+        
+        const req = httpTestingController.expectOne(bugApi.serviceEndPoint);
+        expect(req.request.method).toBe('POST');
+        req.flush({...newBugData, id : 1});
     })
 })

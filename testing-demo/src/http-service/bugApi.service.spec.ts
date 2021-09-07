3f092aadcf7570b39ee641b2b058f4e298f1d480
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-07 16:31:33 +0530

    Update bugApi.service.spec.ts

--- testing-demo/src/http-service/bugApi.service.spec.ts
+++ testing-demo/src/http-service/bugApi.service.spec.ts
@@ -20,6 +20,10 @@ fdescribe('BugApi service', () => {
         httpTestingController = TestBed.inject(HttpTestingController);
     });
 
+    afterEach(() => {
+        httpTestingController.verify();
+    })
+
     it('should be created', () => {
         expect(bugApi).toBeTruthy();
     });
@@ -39,7 +43,7 @@ fdescribe('BugApi service', () => {
         const req = httpTestingController.expectOne(bugApi.serviceEndPoint);
         expect(req.request.method).toBe('GET');
         req.flush(testBugs);
-        httpTestingController.verify();
+        
     });
 
     it('should save a new bug', () => {
@@ -60,5 +64,53 @@ fdescribe('BugApi service', () => {
         const req = httpTestingController.expectOne(bugApi.serviceEndPoint);
         expect(req.request.method).toBe('POST');
         req.flush({...newBugData, id : 1});
+    })
+
+    it("should be able to save an existing bug", () => {
+        const existingBugData : Bug = {
+            id : 1,
+            name : 'Test bug',
+            isClosed : false,
+            createdAt : new Date()
+        }
+
+        bugApi
+            .save(existingBugData)
+            .subscribe(updatedBug => {
+                expect(updatedBug)
+                    .withContext('failed to save new bug')
+                    .toBeTruthy()
+                expect(updatedBug.id)
+                    .withContext('failed to save new bug')
+                    .toBe(1, 'failed to save new bug')
+            });
+
+        const req = httpTestingController.expectOne('http://localhost:3000/bugs/1');
+        expect(req.request.method).toBe('PUT')
+        req.flush({...existingBugData});
+    
+    });
+
+    it("should throw an error when deleting a non existent bug", () => {
+        const bugToRemove = { 
+            id : 300,
+            name : 'Dummy Bug',
+            isClosed : false,
+            createdAt : new Date()
+        };
+
+        bugApi
+            .remove(bugToRemove)
+            .subscribe(() => {
+                fail('should not have been able to delete a non existent bug');
+            }, err => {
+                expect(err).toBeTruthy();
+                expect(err.error.type).toBe('Bug does not exist');
+            });
+        
+        const req = httpTestingController.expectOne(`${bugApi.serviceEndPoint}/300`);
+        expect(req.request.method).toBe('DELETE');
+        req.error(new ErrorEvent('Bug does not exist'));
+
     })
 })

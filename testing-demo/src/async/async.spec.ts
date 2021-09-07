1743d5c85e34064ca5c3d849d0c5c251d79da31d
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-07 17:02:23 +0530

    using fakeAsync & flush

--- testing-demo/src/async/async.spec.ts
+++ testing-demo/src/async/async.spec.ts
@@ -1,3 +1,5 @@
+import { fakeAsync, flush } from "@angular/core/testing";
+
 fdescribe('Async tests', ()=> {
     it('should test for async operations', (done) => {
         function addAsync(x : number,y : number, callback : (result : number) => void){
@@ -13,4 +15,19 @@ fdescribe('Async tests', ()=> {
             done();
         })
     })
+    it("testing multiple async operations", fakeAsync(() => {
+        let test1 = false;
+        setTimeout(function(){
+            test1 = true;
+        }, 3000);
+
+        let test2 = false;
+        setTimeout(function(){
+            test2 = true;
+        }, 5000);
+
+        flush(); //waits for the event queue to be empty (after all the macrotaks are executed)
+        expect(test1).toBe(true);
+        expect(test2).toBe(true);
+    }))
 })

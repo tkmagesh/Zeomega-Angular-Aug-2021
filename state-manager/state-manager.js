6619793872401befb499188458d8914aa05cd112
Magesh Kuppan <tkmagesh77@gmail.com>
2021-09-13 10:45:21 +0530

    state manager - complete

--- state-manager/state-manager.js
+++ state-manager/state-manager.js
@@ -2,6 +2,7 @@ let StateManager = (() =>{
     let _currentState = undefined;
     let _subscribers = [];
     let _reducer = undefined;
+    let _init_action = { type : '@@INIT/ACTION' }
 
     function getState(){
         return _currentState;
@@ -28,6 +29,8 @@ let StateManager = (() =>{
         if (typeof reducer !== 'function') 
             throw new Error('Invalid arguments(s). Reducer is mandatorty to create the store')
         _reducer = reducer;
+        //invoke the reducer to initialize the store with a valid initial state
+        _currentState = _reducer(undefined, _init_action)
         const store =  { getState, subscribe, dispatch };
         return store;
     }
